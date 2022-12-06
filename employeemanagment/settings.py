f94d1f53d0d07360c3aba4599fbe353dd79d0243
anju1511 <anjumathew1511@gmail.com>
2022-12-07 01:58:12 +0530

    templatescreate

--- employeemanagment/settings.py
+++ employeemanagment/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'employeemanagment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
