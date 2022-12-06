d023a9c63b6cedd79831105f7fc089d6dc901658
anju1511 <anjumathew1511@gmail.com>
2022-12-07 02:33:01 +0530

    login

--- employee_app/views.py
+++ employee_app/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 
 # Create your views here.
 
-def index(request):
-    return render(request,'view_employee.html')
+def login(request):
+    if request.method == 'POST':
+        email    = request.POST['email']
+        password = request.POST['password']
+        print(email)
+        print(password)
+        UserModel = get_user_model
+        print(UserModel)
+        username=UserModel.objects.get(email=lower(email)).username
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            
+            # A backend authenticated the credentials
+            return render(request,'view_employee.html')
+        else:
+            # No backend authenticated the credentials
+            context={'message':'email or password incorrect'}
+            return render(request,'login.html',context)
+    else:
+         return render(request,'login.html')
+        
